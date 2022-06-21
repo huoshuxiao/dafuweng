7048e77e2aeba9b93b8aad1fa65adc1b6a5dd71c
wenkunsun <wenkun.sun@accenture.com>
2022-06-21 17:44:56 +0800

    bug fixed [ValueError: dtype='numeric' is not compatible with arrays of bytes/strings.Convert your data to numeric values explicitly instead.]

--- src/main/python/com/sun/dushen/main.py
+++ src/main/python/com/sun/dushen/main.py
@@ -4,8 +4,8 @@ from com.sun.dushen.pixiu import ssq, dlt
 # ssq.run()
 # dlt.run()
 
-# l_model.dlt('22058', '20220525')
-# l_model.ssq('22053', '20220512')
+l_model.dlt(22058, 20220525)
+l_model.ssq(22053, 20220512)
 
 probability.dlt()
 probability.ssq()
