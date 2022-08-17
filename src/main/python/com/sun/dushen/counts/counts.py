4c88e7581e490303394133eccf8185a7f50ba377
wenkun.sun <wenkun.sun@accenture.com>
2022-08-17 17:42:22 +0800

    add new feature counts

---
+++ src/main/python/com/sun/dushen/counts/counts.py
@@ -0,0 +1,45 @@
+from com.sun.dushen.common import utils
+
+
+def ssq(bonuses):
+    for bonus in bonuses:
+        do = True
+        i = 0
+        while do:
+            i = i + 1
+            r = ','.join(str(s) for s in sorted(utils.randoms(33, 6), reverse=False))
+            b = ','.join(str(s) for s in sorted(utils.randoms(16, 1), reverse=False))
+            t = r + ',' + b
+            # print(t)
+            if bonus == t:
+                do = False
+        print(i)
+
+        while i > 0:
+            i = i - 1
+            r = ','.join(str(s) for s in sorted(utils.randoms(33, 6), reverse=False))
+            b = ','.join(str(s) for s in sorted(utils.randoms(16, 1), reverse=False))
+            t = r + ',' + b
+        print(t)
+
+
+def dlt(bonuses):
+    for bonus in bonuses:
+        do = True
+        i = 0
+        while do:
+            i = i + 1
+            r = ','.join(str(s) for s in sorted(utils.randoms(35, 5), reverse=False))
+            b = ','.join(str(s) for s in sorted(utils.randoms(12, 2), reverse=False))
+            t = r + ',' + b
+            # print(t)
+            if bonus == t:
+                do = False
+        print(i)
+
+        while i > 0:
+            i = i - 1
+            r = ','.join(str(s) for s in sorted(utils.randoms(35, 5), reverse=False))
+            b = ','.join(str(s) for s in sorted(utils.randoms(12, 2), reverse=False))
+            t = r + ',' + b
+        print(t)
--- src/main/python/com/sun/dushen/main.py
+++ src/main/python/com/sun/dushen/main.py
@@ -1,11 +1,15 @@
 from com.sun.dushen.lottery import l_model, probability
 from com.sun.dushen.pixiu import ssq, dlt
+from com.sun.dushen.counts import counts
 
 # ssq.run()
 # dlt.run()
 
-probability.dlt()
-l_model.dlt(22070, 20220622)
+# probability.dlt()
+# l_model.dlt(22077, 20220709)
 
-probability.ssq()
-l_model.ssq(22071, 20220623)
+# probability.ssq()
+# l_model.ssq(22080, 20220714)
+
+counts.ssq([['21,22,24,28,29,32,14'], ['6,11,13,16,19,31,2']])
+# counts.dlt([['1,4,12,23,29,5,12'], ['7,11,14,21,29,7,10']])
