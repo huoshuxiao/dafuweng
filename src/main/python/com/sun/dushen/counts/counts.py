de0f9a72c0274b0b1f4d93979b0e7570ce26ea09
wenkun.sun <wenkun.sun@accenture.com>
2022-08-24 13:37:20 +0800

    remove print

--- src/main/python/com/sun/dushen/counts/counts.py
+++ src/main/python/com/sun/dushen/counts/counts.py
@@ -6,7 +6,7 @@ from com.sun.dushen.common import utils
 
 def ssq(bonuses):
     for bonus in bonuses:
-        print(bonus)
+        # print(bonus)
         do = True
         i = 0
         while do:
@@ -18,21 +18,21 @@ def ssq(bonuses):
             if bonus == t:
                 do = False
         print(i)
-        time.sleep(1)
+        # time.sleep(1)
 
         while i > 0:
             i = i - 1
-            print(i)
+            # print(i)
             r = ','.join(str(s) for s in sorted(utils.randoms(33, 6), reverse=False))
             b = ','.join(str(s) for s in sorted(utils.randoms(16, 1), reverse=False))
             t = r + ',' + b
         print(t)
-        time.sleep(1)
+        # time.sleep(1)
 
 
 def dlt(bonuses):
     for bonus in bonuses:
-        print(bonus)
+        # print(bonus)
         do = True
         i = 0
         while do:
@@ -44,13 +44,13 @@ def dlt(bonuses):
             if bonus == t:
                 do = False
         print(i)
-        time.sleep(1)
+        # time.sleep(1)
 
         while i > 0:
             i = i - 1
-            print(i)
+            # print(i)
             r = ','.join(str(s) for s in sorted(utils.randoms(35, 5), reverse=False))
             b = ','.join(str(s) for s in sorted(utils.randoms(12, 2), reverse=False))
             t = r + ',' + b
         print(t)
-        time.sleep(1)
+        # time.sleep(1)
