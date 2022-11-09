727fdbb6f98e55fa834734ee8514cbef35b77f96
DIR\wenkun.sun <wenkun.sun@accenture.com>
2022-11-09 14:17:19 +0800

    根据日期选择类型

--- src/main/python/com/sun/dushen/main.py
+++ src/main/python/com/sun/dushen/main.py
@@ -1,4 +1,4 @@
-import sys
+import datetime
 
 from com.sun.dushen.lottery import l_model, probability
 from com.sun.dushen.pixiu import ssq, dlt
@@ -8,27 +8,31 @@ from com.sun.dushen.counts import counts
 # dlt.run()
 
 
-r"""args 
-    0 is 彩票类型
-"""
-args = sys.argv[1:]
-if 'dlt' == args[0]:
+weekday_dlt = [1, 3, 6, 5]
+weekday_ssq = [2, 4, 7, 5]
+
+date = datetime.date
+today = date.today()
+weekday = today.isoweekday()
+
+if weekday in weekday_dlt:
     l_model.dlt(22128, 20221109)
     probability.dlt()
     counts.dlt2One([
-                    '2,20,28,29,30,7,10',
-                    '2,4,11,25,30,6,12',
-                    '3,7,14,16,19,1,11',
-                    '4,5,8,22,35,1,3',
-                    '2,3,8,9,20,4,10',
-                    ])
-else:
+        '02,20,28,29,30,07,10',
+        '02,04,11,25,30,06,12',
+        '03,07,14,16,19,01,11',
+        '04,05,08,22,35,01,03',
+        '02,03,08,09,20,04,10',
+    ])
+
+if weekday in weekday_ssq:
     l_model.ssq(22129, 20221110)
     probability.ssq()
     counts.ssq2One([
-                    '5,10,13,18,24,26,1',
-                    '2,3,7,12,20,31,16',
-                    '1,13,15,17,26,33,13',
-                    '3,4,9,10,29,33,13',
-                    '3,12,18,24,27,29,1',
-                    ])
+        '05,10,13,18,24,26,01',
+        '02,03,07,12,20,31,16',
+        '01,13,15,17,26,33,13',
+        '03,04,09,10,29,33,13',
+        '03,12,18,24,27,29,01',
+    ])
